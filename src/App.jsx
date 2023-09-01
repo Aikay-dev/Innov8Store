5efc49089e4c403d35124e780a605dcc695ca139
Emmanuel Festus <120015643+GeneralAike@users.noreply.github.com>
2023-09-01 12:22:12 +0100

    added link to learn github

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>AMAZON BY INNOVATE SPACE</h1>
+      <a href="https://www.youtube.com/watch?v=8Dd7KRpKeaE&pp=ygUUbGVhcm4gZ2l0IGFuZCBnaXRodWI%3D">
+        LEARN GIT AND GITHUB FROM YOUTUBE
+      </a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
